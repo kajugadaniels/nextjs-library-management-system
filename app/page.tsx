ff66690739ac30fd8dc34218e6ef6f311fb6babf
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-13 18:11:31 +0200

    New changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-    return (
-        <>
-            Hello World
-        </>
-    );
+  return (
+    <>
+      Hello World!!!
+      <Button />
+    </>
+  );
 }
