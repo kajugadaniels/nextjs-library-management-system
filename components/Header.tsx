51e9522b9cf7639c63d6d2ff4adcb0a11a4cdc2e
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-14 12:25:44 +0200

    Design header component

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const Header = () => {
+  const pathname = usePathname();
+  return (
+    <header className="my-10 flex justify-between gap-5">
+      Header
+      <Link href="/">
+        <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
+      </Link>
+      <ul className="flex flex-row items-center gap-8">
+        <li>
+          <Link
+            href="/library"
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathname == "/library" ? "text-light-200" : "text-light-100",
+            )}
+          >
+            Library
+          </Link>
+        </li>
+        <li>
+          <form className="mb-10">
+            <Button>Logout</Button>
+          </form>
+        </li>
+      </ul>
+    </header>
+  );
+};
+export default Header;
