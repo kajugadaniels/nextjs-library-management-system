9d5c36b7f5d2498f27fe4052fd11fea0ab2eaedc
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-15 12:04:32 +0200

    Design book cover component

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,6 +1,51 @@
 import React from "react";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
-const BookCover = () => {
-  return <div>BookCover</div>;
+type BookCoverVriant = "extraSmall" | "small" | "medium" | "regular" | "wide";
+
+const variantStyles: Record<BookCoverVriant, string> = {
+  extraSmall: "book-cover-extra_small",
+  small: "book-cover_small",
+  medium: "book-cover_medium",
+  regular: "book-cover_regular",
+  wide: "book-cover_wide",
+};
+
+interface Props {
+  className?: string;
+  variant?: BookCoverVriant;
+  coverColor: string;
+  coverImage: string;
+}
+
+const BookCover = ({
+  className,
+  variant = "regular",
+  coverColor = "#012B48",
+  coverImage = "https://placehold.co/400x600.png",
+}: Props) => {
+  return (
+    <div
+      className={cn(
+        "relative transition-all duration-300",
+        variantStyles[variant],
+        className,
+      )}
+    >
+      Book Side SVG
+      <div
+        className="absolute z-10"
+        style={{ left: "12%", width: "87.5%", height: "88%" }}
+      >
+        <Image
+          src={coverImage}
+          alt="Book cover"
+          fill
+          className="rounded-sm object-fill"
+        />
+      </div>
+    </div>
+  );
 };
 export default BookCover;
