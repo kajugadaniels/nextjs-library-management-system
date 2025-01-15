f81c1f576f091541514890188df6082820016ddd
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-15 11:44:21 +0200

    Design bookOverview component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
 
 const Page = () => {
   return (
     <>
-      <BookOverview />
+      <BookOverview {...sampleBooks[0]} />
 
-      <BookList />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </>
   );
 };
---
+++ components/BookCover.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const BookCover = () => {
+  return <div>BookCover</div>;
+};
+export default BookCover;
