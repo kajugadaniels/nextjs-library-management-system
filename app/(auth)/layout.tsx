99e78f04f389a90088b8cc520467ebff64e091e2
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-15 16:35:23 +0200

    Setup auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import Image from "next/image";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
@@ -10,6 +10,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
             <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
             <h1 className="text-2xl font-semibold text-white">BookWise</h1>
           </div>
+
           <div>{children}</div>
         </div>
       </section>
@@ -26,4 +27,5 @@ const Layout = async ({ children }: { children: ReactNode }) => {
     </main>
   );
 };
+
 export default Layout;
